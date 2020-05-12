247cccabaaf42bee5c17f53814ab6ad5fa448ef2
rocco-de-luca <roccodelucaboolean@gmail.com>
2020-05-12 18:45:15 +0200

    html-sass-first

---
+++ dist/js/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+console.log('js inserted');
+})();
+
+(() => {
+/*!****************************!*\
+  !*** ./src/scss/main.scss ***!
+  \****************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+// extracted by mini-css-extract-plugin
+})();
+
+/******/ })()
+;
